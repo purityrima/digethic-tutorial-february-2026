e95834a4233f743ccffd53c71133bba4649fb95e
Purity  Rima <purityrima atgmail.com>
2026-05-05 14:30:34 +0300

    DIG-771: Set up pytest framework, add unit tests and configure pythonpath

--- src/hello.py
+++ src/hello.py
@@ -1,1 +1,2 @@
-print("hello world")
+def hello():
+    return "Hello World"
---
+++ tests/test_math.py
@@ -0,0 +1,9 @@
+from src.math import add, multiply
+
+
+def test_add():
+    assert add(2, 3) == 5
+
+
+def test_multiply():
+    assert multiply(2, 3) == 6
