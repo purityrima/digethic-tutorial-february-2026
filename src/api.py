9d27ff08ddd6399bb1c7830bf7e9fce1b261a4ea
Purity  Rima <purityrima atgmail.com>
2026-05-08 00:19:29 +0300

    DIG-782: Add prediction endpoint to Flask API

--- src/api.py
+++ src/api.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import (
+    Flask,
+    request,
+)  # We need "request" because "request.args.get()" comes from Flask’s request object.
 from flask_cors import CORS
+
 import pandas as pd
+import pickle
+
+with open(
+    "data/models/baumethoden_lr.pickle", "rb"
+) as file:  # rb = read binary, because pickle files are binary files, not text files.
+    trained_model = pickle.load(file)  # pickle.load is used
+
+# Task 2
 
 app = Flask(__name__)  # creates the Flask application
 CORS(app)  # allows cross-origin requests.
@@ -22,3 +34,23 @@ def training_data():
         "data/auto-mpg-training-data.csv", sep=";"
     )  # reads yourthe CSV and converts it into JSON-style records:
     return data.to_dict(orient="records")  # which Flask then returns as JSON.
+
+
+# Task 3
+@app.route("/predict", methods=["GET"])
+def predict():
+    zylinder = float(request.args.get("zylinder"))
+    ps = float(request.args.get("ps"))
+    gewicht = float(request.args.get("gewicht"))
+    beschleunigung = float(request.args.get("beschleunigung"))
+    baujahr = float(request.args.get("baujahr"))
+
+    print(
+        f"Received request with zylinder: {zylinder}, ps: {ps}, gewicht: {gewicht}, beschleunigung: {beschleunigung}, baujahr: {baujahr}"
+    )
+
+    prediction_data = [[zylinder, ps, gewicht, beschleunigung, baujahr]]
+    # prediction_data = [[ps, gewicht]]
+    prediction = trained_model.predict(prediction_data)
+
+    return {"result": float(prediction[0])}
--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+import matplotlib.pyplot as plt
 import pickle
 import os
 
@@ -11,18 +12,17 @@ print(data.head())
 
 # Shuffle data. It randomizes the rows. models learn better when data is mixed.
 # frac=1 = take 100% of data but shuffled
-data = data.sample(frac=1, random_state=42)
+data = data.sample(frac=1)
 
 # Class column. mpg means or is miles per gallon. 1 US gallon `~ 3.8 liters`
 y_variable = data["mpg"]
 
 # Feature columns
-# x_variables = data.loc[:, data.columns != "mpg"]
-x_variables = data[["horsepower", "weight"]]
+x_variables = data.loc[:, data.columns != "mpg"]
 
 # Split data
 x_train, x_test, y_train, y_test = train_test_split(
-    x_variables, y_variable, test_size=0.2, random_state=42
+    x_variables, y_variable, test_size=0.2
 )
 
 # Train model
@@ -41,11 +41,11 @@ print("MAE:", mean_absolute_error(y_test, y_pred))
 print("MSE:", mean_squared_error(y_test, y_pred))
 
 # Visualize the output
-# plt.scatter(y_test, y_pred)
-# plt.xlabel("Actual MPG")
-# plt.ylabel("Predicted MPG")
-# plt.title("Actual vs Predicted MPG")
-# plt.show()
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual MPG")
+plt.ylabel("Predicted MPG")
+plt.title("Actual vs Predicted MPG")
+plt.show(block=False)
 # Ensure models folder exists
 os.makedirs("data/models", exist_ok=True)
 
